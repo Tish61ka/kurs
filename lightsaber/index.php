6431beb33499d595afcddd9e92c2629cadfd00e2
Tish61ka <49403327+Tish61ka@users.noreply.github.com>
2022-03-10 13:56:15 +0400

    Add files via upload

---
+++ lightsaber/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>LightSaber Shop</title>
+</head>
+<body>
+    <header>
+        <img src="img/png-transparent-brand-line-angle-font-lightsaber-text-logo-maker-thumbnail.png" alt="">
+        <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">Каталог</a></li>
+        </ul>
+        <a class="button" href="#"><button>Вход</button></a>
+    </header>
+</body>
+</html>
